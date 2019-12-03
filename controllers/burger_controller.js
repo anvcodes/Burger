527fcd5b78045cca13529ec46a08f7abb9413887
anvcodes <kpeace0720@gmail.com>
2019-12-03 17:42:00 -0500

    heroku deployed

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,21 +1,20 @@
-// Inside the burgers_controller.js file, import the following:
 // Express
 var express = require("express");
 
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
+// Import the model from burger.js to use database functions.
 var burger = require("../models/burger.js");
 
 // Create the router for the app, and export the router at the end of your file.
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
     burger.selectAll(function(data) {
-      var hbsObject = {
-        burger: data
+      var burgerObject = {
+        burger: req.data
       };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
+      console.log(burgerObject);
+      res.render("index", burgerObject);
     });
   });
   
